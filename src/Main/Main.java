2421f3a3c5fbfc8d4be82588d1e34aab67047d72
IlReSenzaNome <alexisvivas3@gmail.com>
2023-11-21 20:02:02 -0500

    Practice main interfaces

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,27 +9,6 @@ public class Main {
     public static void main(String[] args) {
         BankUI bankui = new BankUI();
         bankui.setVisible(true);
-       
-        /*BankLogic bankLogic = new BankLogic();
-        bankui.setLocationRelativeTo(null);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your password: ");
-
-        try {
-            bankLogic.setPassword(scanner.nextInt());
-            boolean isPasswordValid = bankLogic.validatePassword();
-
-            if (isPasswordValid) {
-                int option = bankLogic.rotulo();
-                bankLogic.selection(option);
-            } else {
-                System.out.println("Your password is invalid!");
-            }
-        } catch (Exception e) {
-            System.out.println("Input a numeric password");
-            System.exit(0);
-        }*/
-
     }
 
 }
